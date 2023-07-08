60b81db2596f2f18451138c2067c1136c4db6380
painnick <painnick@gmail.com>
2023-07-08 23:45:18 +0900

    UPDATE.

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,102 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
 
 #include "esp_log.h"
 
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-i2c-communication-arduino-ide/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
+#define MAIN_TAG "main"
 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#define PIN_SERVO 27
+// #define PIN_OLED_SDA 21
+// #define PIN_OLED_SDA 22
+#define TOUCH_UP T5 // 12
+#define TOUCH_DOWN T6 // 14
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+#define CHECK_INTERVAL_SERVO_MS 500
+#define CHECK_INTERVAL_DISPLAY_MS 1000
+
+#define DEGREES_MAX 180
+#define DEGREES_MIN 0
+
+Servo servo;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void setup() {
-  Serial.begin(115200);
+int servoDegrees{};
+int lastServoDegrees{};
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+void setup() {
+  ESP_LOGI(MAIN_TAG, "Connecting OLED...");
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    ESP_LOGE(MAIN_TAG, "SSD1306 allocation failed");
+    for (;;)
+      ;
   }
-  
+  ESP_LOGI(MAIN_TAG, "OLED is Connected.");
+
   display.clearDisplay();
-  display.setTextColor(WHITE);
+  display.setTextColor(WHITE);  // Fixed
+
+  servo.attach(PIN_SERVO);
+  servo.write(servoDegrees);
+  lastServoDegrees = servoDegrees;
 }
 
+unsigned long lastServoChecked{};
+unsigned long lastDisplayChecked{};
 void loop() {
-  display.clearDisplay();
-  // display temperature
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.print("Temperature: ");
-  display.setTextSize(2);
-  display.setCursor(0,10);
-  display.print(String(rand() % 10000));
-  display.print(" ");
-  display.setTextSize(1);
-  display.cp437(true);
-  display.write(167);
-  display.setTextSize(2);
-  display.print("C");
-  
-  // display humidity
-  display.setTextSize(1);
-  display.setCursor(0, 35);
-  display.print("Humidity: ");
-  display.setTextSize(2);
-  display.setCursor(0, 45);
-  display.print(String(String(rand() % 10000)));
-  display.print(" %"); 
-  
-  display.display();
+  int touchUp = touchRead(TOUCH_UP);
+  ESP_LOGV(MAIN_TAG, "Up : %d", touchUp);
+  if (touchUp < 50) {
+    servoDegrees = min(DEGREES_MAX, servoDegrees + 1);
+  }
+  int touchDown = touchRead(TOUCH_DOWN);
+  ESP_LOGV(MAIN_TAG, "Down : %d", touchDown);
+  if (touchDown < 50) {
+    servoDegrees = max(DEGREES_MIN, servoDegrees - 1);
+  }
 
-  delay(1000);
+  unsigned long now = millis();
+  if ((lastServoDegrees != servoDegrees) &&
+      (now - lastServoChecked > CHECK_INTERVAL_SERVO_MS)) {
+    servo.write(servoDegrees);
+    ESP_LOGI(MAIN_TAG, "Degrees : %d", servoDegrees);
+    lastServoDegrees = servoDegrees;
+    lastServoChecked = now;
+  }
+
+  if (now - lastDisplayChecked > CHECK_INTERVAL_DISPLAY_MS) {
+    display.clearDisplay();
+
+    // display temperature
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.print("Degrees :");
+
+    display.setTextSize(2);
+    display.setCursor(0, 10);
+    if (servoDegrees < 100) {
+      display.print(" ");
+      if (servoDegrees < 10) {
+        display.print(" ");
+      }
+    }
+    display.print(String(servoDegrees));
+    // https://en.wikipedia.org/wiki/Code_page_437
+    display.setTextSize(1);
+    display.cp437(true);
+    display.write(0xA7);
+
+    display.display();
+
+    lastDisplayChecked = now;
+  }
+
+  delay(50);
 }
